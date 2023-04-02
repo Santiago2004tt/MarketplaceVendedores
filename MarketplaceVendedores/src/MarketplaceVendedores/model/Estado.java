037652a3ee8313bccfb62862621bfce6b3f23061
unknown <huendycaicedo@gmail.comg>
2023-04-02 18:11:25 -0500

    documentacion 1

--- MarketplaceVendedores/src/MarketplaceVendedores/model/Estado.java
+++ MarketplaceVendedores/src/MarketplaceVendedores/model/Estado.java
@@ -1,4 +1,10 @@
 package MarketplaceVendedores.model;
 
+/**
+ * Enum del estado del producto
+ * ----------English-----------
+ * Enum of the state of the product
+ */
 public enum Estado {
+    PUBLICADO, VENDIDO, CANCELADO, AGOTADO
 }
--- MarketplaceVendedores/src/MarketplaceVendedores/model/MuroInterface.java
+++ MarketplaceVendedores/src/MarketplaceVendedores/model/MuroInterface.java
@@ -1,7 +1,18 @@
 package MarketplaceVendedores.model;
 
+/**
+ * interfase de muro
+ * -----English-----
+ * Wall interface
+ */
 public interface MuroInterface {
 
+    /**
+     * usos
+     * ---
+     * usages
+     * @param mensaje
+     */
     void aniadirMensaje(String mensaje);
     boolean verificarMensaje(String mensaje);
 }
