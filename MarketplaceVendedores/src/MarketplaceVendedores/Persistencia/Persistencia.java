8696e5af64ad8d13b68e072bd801f578a73ecae5
Miguel Garcia <100101000+lordthar@users.noreply.github.com>
2023-04-12 22:03:21 -0500

    Creacion Persisitencia y ArchivoUtil

---
+++ MarketplaceVendedores/src/MarketplaceVendedores/Persistencia/Persistencia.java
@@ -0,0 +1,38 @@
+package MarketplaceVendedores.Persistencia;
+
+import MarketplaceVendedores.model.Vendedor;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Persistencia {
+    public static final String RUTA_ARCHIVO_EMPLEADOS = "src/resources/archivoEmpleados.txt";
+
+    public static void guardarEmpleados(ArrayList<Vendedor> listaVendedores) throws IOException {
+
+        String contenido = "";
+
+        for (Vendedor vendedor : listaVendedores) {
+            contenido += vendedor.getNombre() +"\n";
+        }
+        ArchivoUtil.guardarArchivo(RUTA_ARCHIVO_EMPLEADOS, contenido, false);
+    }
+
+//    ----------------------LOADS------------------------
+
+    private static ArrayList<Vendedor> cargarVendedores() throws IOException {
+
+        ArrayList<Vendedor> vendedores = new ArrayList<Vendedor>();
+
+        ArrayList<String> contenido = ArchivoUtil.leerArchivo(RUTA_ARCHIVO_EMPLEADOS);
+        String linea = "";
+
+        for (int i = 0; i < contenido.size(); i++) {
+            linea = contenido.get(i);
+            Vendedor vendedor = new Vendedor();
+            vendedor.setNombre(linea.split(",")[0]);
+            vendedores.add(vendedor);
+        }
+        return vendedores;
+    }
+}
