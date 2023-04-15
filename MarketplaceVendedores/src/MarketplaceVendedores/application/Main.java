17786d3ffe374baf0f7951e2ce2ee98adcc66ec9
Santiago2004tt <santiago.sepulvedab@uqvirtual.edu.co>
2023-04-14 19:45:42 -0500

    Crear carpetas y vincular

--- MarketplaceVendedores/src/MarketplaceVendedores/application/Main.java
+++ MarketplaceVendedores/src/MarketplaceVendedores/application/Main.java
@@ -1,7 +1,15 @@
 package MarketplaceVendedores.application;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
     }
 }
---
+++ MarketplaceVendedores/src/MarketplaceVendedores/controllers/controller.java
@@ -0,0 +1,4 @@
+package MarketplaceVendedores.controllers;
+
+public class controller {
+}
